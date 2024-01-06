6e8b354e668d514807f7db9c845958bdca4f1e15
Nico Makowe <Nico.Makowe01@gmail.com>
2024-01-06 19:14:07 +0100

    implement saving and updating of routines

--- lib/services/routine_service.dart
+++ lib/services/routine_service.dart
@@ -4,30 +4,71 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 import '../classes/routine.dart';
+import '../constants/element_list_pommel_horse.dart';
+
+Routine sampleRoutine1 = Routine(
+    name: 'Beispielübung',
+    elements: [
+      p_1_1,
+      p_1_3,
+      p_1_7,
+      p_1_8,
+      p_1_13,
+      p_2_1,
+      p_2_92,
+      p_3_1,
+    ]
+);
+Routine sampleRoutine2 = Routine(
+    elements: [
+      p_1_1,
+    ]
+);
 
 late Future<Database> futureDb;
 
 Future<void> initRoutinesDb() async {
-  futureDb = openDatabase(join(await getDatabasesPath(), 'elements.db'),
+  futureDb = openDatabase(join(await getDatabasesPath(), 'routines.db'),
       onCreate: createRoutinesTable, version: 1);
 }
 
-Future<void> createRoutinesTable(Database elementsDb, int version) async {
-  elementsDb.execute('CREATE TABLE routines('
+Future<void> createRoutinesTable(Database db, int version) async {
+  await db.execute('CREATE TABLE routines('
       'id INTEGER PRIMARY KEY,'
       'name TEXT,'
       'elements TEXT)');
+  await _storeNewRoutine(sampleRoutine1, db);
+  await _storeNewRoutine(sampleRoutine2, db);
 }
 
-Future<void> addRoutineToDb(Routine routine) async {
-  Database db = await futureDb;
-  await db.insert('routines', routine.toMap());
+
+Future<void> storeRoutine(Routine routine) async {
+  if(routine.id != null) {
+    // routine is already existing -> update
+    return _updateExistingRoutine(routine);
+  }
+  else {
+    // routine is newly created -> store
+    int id = await _storeNewRoutine(routine);
+    routine.id = id;
+  }
 }
+
+Future<int> _storeNewRoutine(Routine routine, [Database? db]) async {
+  db ??= await futureDb;
+  return await db.insert('routines', routine.toMap());
+}
+
+Future<void> _updateExistingRoutine(Routine routine) async {
+  Database db = await futureDb;
+  await db.update('routines', routine.toMap(), where: 'id = ?', whereArgs: [routine.id]);
+}
+
 
 Future<List<Routine>> getAllRoutines() async {
+  Database db = await futureDb;
   List<Future<Routine>> futureRoutines = [];
 
-  Database db = await futureDb;
   final List<Map<String, dynamic>> maps = await db.query('routines');
   for (Map<String, dynamic> map in maps) {
     futureRoutines.add(Routine.fromMap(map));
